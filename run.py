7317ee17ce5f80ad7ca793e67829dc5b5b1a3d7e
tecari0 <121329615+tecari0@users.noreply.github.com>
2023-09-23 14:58:42 -0500

    bruh

---
+++ run.py
@@ -0,0 +1,4 @@
+from HackRice13.main import client, discord_token
+
+if __name__ == "__main__": #check if the program is run as a main program, or import as a module. Need to run the main program
+    client.run(discord_token)
